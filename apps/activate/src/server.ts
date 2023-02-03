ffda4d5453e17d50eef352a677a078465fa6e185
Leng, Errong <eleng@bestbuycanada.ca>
2023-02-03 01:31:32 -0800

    Vite SSR css extract load

--- apps/activate/src/server.ts
+++ apps/activate/src/server.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export async function createServer(
   root = process.cwd(),
-  hmrPort = 4200,
+  hmrPort = 4224,
 ) {
   const resolve = (p: string) => path.resolve(__dirname, p)
 
@@ -17,38 +17,47 @@ export async function createServer(
    * @type {import('vite').ViteDevServer}
    */
   const vite = await (
-      await import('vite')
-    ).createServer({
-      root,
-      logLevel: 'info',
-      server: {
-        middlewareMode: true,
-        watch: {
-          // During tests we edit the files too fast and sometimes chokidar
-          // misses change events, so enforce polling for consistency
-          usePolling: true,
-          interval: 100,
-        },
-        hmr: {
-          port: hmrPort,
-        },
+    await import('vite')
+  ).createServer({
+    root,
+    logLevel: 'info',
+    server: {
+      middlewareMode: true,
+      watch: {
+        // During tests we edit the files too fast and sometimes chokidar
+        // misses change events, so enforce polling for consistency
+        usePolling: true,
+        interval: 100,
       },
-      appType: 'custom',
+      hmr: {
+        port: hmrPort,
+      },
+    },
+    appType: 'custom',
+  })
+  // use vite's connect instance as middleware
+  app.use(vite.middlewares)
+
+  // serve the app source root as static resources
+  // so that we can easily load styles.css from index.html
+  app.get(
+    "*.*",
+    express.static(path.join(process.cwd(), "apps/activate"), {
+      maxAge: "1d",
     })
-    // use vite's connect instance as middleware
-    app.use(vite.middlewares)
+  );
 
   app.use('*', async (req, res) => {
     try {
       const url = req.originalUrl
 
       let template;
-     
-        // always read fresh template in dev
-        template = fs.readFileSync(resolve('../index.html'), 'utf-8')
-        template = await vite.transformIndexHtml(url, template)
+
+      // always read fresh template in dev
+      template = fs.readFileSync(resolve('../index.html'), 'utf-8')
+      template = await vite.transformIndexHtml(url, template)
       const { render } = (await vite.ssrLoadModule('/apps/activate/src/main.server.tsx'));
-      
+
       const appHtml = render()
       const html = template.replace(`<!--app-html-->`, appHtml)
 
@@ -63,9 +72,9 @@ export async function createServer(
   return { app, vite }
 }
 
-const port = process.env.PORT || 3000;
-  createServer().then(({ app }) => {
-    const server = app.listen(port, () => {
+const port = process.env.PORT || 4200;
+createServer().then(({ app }) => {
+  const server = app.listen(port, () => {
     // Server has started
     console.log(`app listening on port ${port}`);
   });
