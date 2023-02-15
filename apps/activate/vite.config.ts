e263712f5ba9f858b116ffa12947885957fb8c19
Leng, Errong <eleng@bestbuycanada.ca>
2023-02-14 16:08:31 -0800

    generate common js to be executed by js:node

--- apps/activate/vite.config.ts
+++ apps/activate/vite.config.ts
@@ -40,4 +40,13 @@ export default defineConfig({
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
+
+  build: {
+    rollupOptions: {
+      output: {
+        format: "commonjs"
+      }
+    }
+  }
+
 });
